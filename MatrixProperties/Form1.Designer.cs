38db142cfde338880e46f00c77ee62938d90944e
Anas <a_n_a_s@inbox.ru>
2023-03-11 20:52:01 +0300

    add

---
+++ MatrixProperties/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿
+namespace MatrixProperties
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.for_M = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // for_M
+            // 
+            this.for_M.Location = new System.Drawing.Point(112, 135);
+            this.for_M.Name = "for_M";
+            this.for_M.Size = new System.Drawing.Size(47, 20);
+            this.for_M.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 326);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Размер матрицы MxM";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(84, 138);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(22, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "M=";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 40);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(130, 37);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Построить матрицу MxM";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(6, 96);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(130, 37);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Разложение матрицы";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(6, 154);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(130, 37);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Свойства матрицы";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(405, 302);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(84, 37);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Ок";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Location = new System.Drawing.Point(193, 22);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(147, 224);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Обратные";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button5);
+            this.groupBox2.Controls.Add(this.button6);
+            this.groupBox2.Controls.Add(this.button7);
+            this.groupBox2.Location = new System.Drawing.Point(366, 22);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(147, 224);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Псевдообратные";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(6, 40);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(130, 37);
+            this.button5.TabIndex = 3;
+            this.button5.Text = "Построить матрицу MxM";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(6, 96);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(130, 37);
+            this.button6.TabIndex = 4;
+            this.button6.Text = "Разложение матрицы";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(6, 154);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(130, 37);
+            this.button7.TabIndex = 5;
+            this.button7.Text = "Свойства матрицы";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(571, 380);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.for_M);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox for_M;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+    }
+}
+
---
+++ MatrixProperties/Form1.cs
@@ -0,0 +1,287 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MatrixProperties
+{
+
+    public partial class Form1 : Form
+    {
+        private int M;
+        private double[,] A1;
+        private double[,] A2;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for_M.Text = "3";
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.White;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)//построение обратной матрицы
+        {
+            M = Convert.ToInt32(for_M.Text);
+            A1 = new double[M,M];
+            double det=0;//определитель матрицы
+            while(det==0)
+            {
+                Random rnd = new Random();
+                for(int i=0;i<M;i++)
+                {
+                    for (int j = 0; j < M; j++)
+                    {
+                        A1[i,j] = (rnd.NextDouble() - 1);
+                    }
+                }
+                det=Determ(A1); 
+            }
+            for (int row = 0; row < A1.GetLength(0); row++)
+            {
+                for (int column = 0; column < A1.GetLength(1); column++)
+                    Console.Write(A1[row, column] + "\t");
+                Console.WriteLine();
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)//разложение обратной матрицы
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)//свойства обратной матрицы
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)//построение псевдообратной матрицы
+        {
+            M = Convert.ToInt32(for_M.Text);
+            A2 = new double[M, M];
+        }
+
+        private void button6_Click(object sender, EventArgs e)//разложение псевдообратной матрицы
+        {
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)//свойства псевдообратной матрицы
+        {
+
+        }
+        private void button4_Click(object sender, EventArgs e)//ок
+        {
+
+        }
+        //для вычисления определителя
+        public static double[,] GetMinor(double[,] matrix, int row, int column)
+        {
+            if (matrix.GetLength(0) != matrix.GetLength(1)) throw new Exception(" Число строк в матрице не совпадает с числом столбцов");
+            double[,] buf = new double[matrix.GetLength(0) - 1, matrix.GetLength(0) - 1];
+            for (int i = 0; i < matrix.GetLength(0); i++)
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if ((i != row) || (j != column))
+                    {
+                        if (i > row && j < column) buf[i - 1, j] = matrix[i, j];
+                        if (i < row && j > column) buf[i, j - 1] = matrix[i, j];
+                        if (i > row && j > column) buf[i - 1, j - 1] = matrix[i, j];
+                        if (i < row && j < column) buf[i, j] = matrix[i, j];
+                    }
+                }
+            return buf;
+        }
+        public static double Determ(double[,] matrix)
+        {
+            if (matrix.GetLength(0) != matrix.GetLength(1)) throw new Exception(" Число строк в матрице не совпадает с числом столбцов");
+            double det = 0;
+            int Rank = matrix.GetLength(0);
+            if (Rank == 1) det = matrix[0, 0];
+            if (Rank == 2) det = matrix[0, 0] * matrix[1, 1] - matrix[0, 1] * matrix[1, 0];
+            if (Rank > 2)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    det += Math.Pow(-1, 0 + j) * matrix[0, j] * Determ(GetMinor(matrix, 0, j));
+                }
+            }
+            return det;
+        }
+    }
+
+    //class Matrix
+    //{
+    //    // Скрытые поля
+    //    private int n;
+    //    private int[,] mass;
+
+    //    // Создаем конструкторы матрицы
+    //    public Matrix() { }
+    //    public int N
+    //    {
+    //        get { return n; }
+    //        set { if (value > 0) n = value; }
+    //    }
+
+    //    // Задаем аксессоры для работы с полями вне класса Matrix
+    //    public Matrix(int n)
+    //    {
+    //        this.n = n;
+    //        mass = new int[this.n, this.n];
+    //    }
+    //    public int this[int i, int j]
+    //    {
+    //        get
+    //        {
+    //            return mass[i, j];
+    //        }
+    //        set
+    //        {
+    //            mass[i, j] = value;
+    //        }
+    //    }
+
+    //    // Ввод матрицы с клавиатуры
+    //    public void WriteMat()
+    //    {
+    //        for (int i = 0; i < n; i++)
+    //        {
+    //            for (int j = 0; j < n; j++)
+    //            {
+    //                Console.WriteLine("Введите элемент матрицы {0}:{1}", i + 1, j + 1);
+    //                mass[i, j] = Convert.ToInt32(Console.ReadLine());
+    //            }
+    //        }
+    //    }
+
+    //    // Вывод матрицы с клавиатуры
+    //    public void ReadMat()
+    //    {
+    //        for (int i = 0; i < n; i++)
+    //        {
+    //            for (int j = 0; j < n; j++)
+    //            {
+    //                Console.Write(mass[i, j] + "\t");
+    //            }
+    //            Console.WriteLine();
+    //        }
+    //    }
+
+
+    //    // Проверка матрицы А на единичность
+    //    public void oneMat(Matrix a)
+    //    {
+    //        int count = 0;
+    //        for (int i = 0; i < n; i++)
+    //        {
+    //            for (int j = 0; j < n; j++)
+    //            {
+    //                if (a[i, j] == 1 && i == j)
+    //                {
+    //                    count++;
+    //                }
+    //            }
+
+    //        }
+    //        if (count == a.N)
+    //        {
+    //            Console.WriteLine("Единичная");
+    //        }
+    //        else Console.WriteLine("Не единичная");
+    //    }
+
+
+    //    // Умножение матрицы А на число
+    //    public static Matrix umnch(Matrix a, int ch)
+    //    {
+    //        Matrix resMass = new Matrix(a.N);
+    //        for (int i = 0; i < a.N; i++)
+    //        {
+    //            for (int j = 0; j < a.N; j++)
+    //            {
+    //                resMass[i, j] = a[i, j] * ch;
+    //            }
+    //        }
+    //        return resMass;
+    //    }
+
+    //    // Умножение матрицы А на матрицу Б
+    //    public static Matrix umn(Matrix a, Matrix b)
+    //    {
+    //        Matrix resMass = new Matrix(a.N);
+    //        for (int i = 0; i < a.N; i++)
+    //            for (int j = 0; j < b.N; j++)
+    //                for (int k = 0; k < b.N; k++)
+    //                    resMass[i, j] += a[i, k] * b[k, j];
+
+    //        return resMass;
+    //    }
+
+
+
+    //    // перегрузка оператора умножения
+    //    public static Matrix operator *(Matrix a, Matrix b)
+    //    {
+    //        return Matrix.umn(a, b);
+    //    }
+
+    //    public static Matrix operator *(Matrix a, int b)
+    //    {
+    //        return Matrix.umnch(a, b);
+    //    }
+
+
+    //    // Метод вычитания матрицы Б из матрицы А
+    //    public static Matrix razn(Matrix a, Matrix b)
+    //    {
+    //        Matrix resMass = new Matrix(a.N);
+    //        for (int i = 0; i < a.N; i++)
+    //        {
+    //            for (int j = 0; j < b.N; j++)
+    //            {
+    //                resMass[i, j] = a[i, j] - b[i, j];
+    //            }
+    //        }
+    //        return resMass;
+    //    }
+
+    //    // Перегрузка оператора вычитания
+    //    public static Matrix operator -(Matrix a, Matrix b)
+    //    {
+    //        return Matrix.razn(a, b);
+    //    }
+    //    public static Matrix Sum(Matrix a, Matrix b)
+    //    {
+    //        Matrix resMass = new Matrix(a.N);
+    //        for (int i = 0; i < a.N; i++)
+    //        {
+    //            for (int j = 0; j < b.N; j++)
+    //            {
+    //                resMass[i, j] = a[i, j] + b[i, j];
+    //            }
+    //        }
+    //        return resMass;
+    //    }
+    //    // Перегрузка сложения
+    //    public static Matrix operator +(Matrix a, Matrix b)
+    //    {
+    //        return Matrix.Sum(a, b);
+    //    }
+    //    // Деструктор Matrix
+    //    ~Matrix()
+    //    {
+    //        Console.WriteLine("Очистка");
+    //    }
+
+    //}
+}
